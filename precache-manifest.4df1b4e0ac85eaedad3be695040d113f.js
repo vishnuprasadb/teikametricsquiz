033e25bcbb41826167d095f371c1b758f1f1d4b9
flo <prasadbajadka@gmail.com>
2020-07-26 14:56:21 +0530

    Updates

---
+++ precache-manifest.4df1b4e0ac85eaedad3be695040d113f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "408d84102784e1faf5686b21d5b415a9",
+    "url": "/teikametricsquiz/index.html"
+  },
+  {
+    "revision": "b62a1315f28fbbb03901",
+    "url": "/teikametricsquiz/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c90707ba6f59d7096842",
+    "url": "/teikametricsquiz/static/js/2.a0f24a1c.chunk.js"
+  },
+  {
+    "revision": "ba3b4500f00880cf9656c39900077397",
+    "url": "/teikametricsquiz/static/js/2.a0f24a1c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b62a1315f28fbbb03901",
+    "url": "/teikametricsquiz/static/js/main.b2b532ab.chunk.js"
+  },
+  {
+    "revision": "0ee9a200a25330687ec9",
+    "url": "/teikametricsquiz/static/js/runtime-main.80719216.js"
+  }
+]);
