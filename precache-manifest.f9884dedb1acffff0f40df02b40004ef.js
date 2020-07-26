3164b1958ff5316a0bfcde34e1d9d4edd7e35826
flo <prasadbajadka@gmail.com>
2020-07-26 16:12:50 +0530

    Updates

---
+++ precache-manifest.f9884dedb1acffff0f40df02b40004ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f17082653a3d101aa64b09e6311c9484",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70f2b3bbd2bdd39238ce",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c90707ba6f59d7096842",
+    "url": "/static/js/2.a0f24a1c.chunk.js"
+  },
+  {
+    "revision": "ba3b4500f00880cf9656c39900077397",
+    "url": "/static/js/2.a0f24a1c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70f2b3bbd2bdd39238ce",
+    "url": "/static/js/main.2ade7968.chunk.js"
+  },
+  {
+    "revision": "c2f4b93197629ec73ace",
+    "url": "/static/js/runtime-main.5ca581f5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/teikametricsquiz/precache-manifest.4df1b4e0ac85eaedad3be695040d113f.js"
+  "/precache-manifest.f9884dedb1acffff0f40df02b40004ef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/teikametricsquiz/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
