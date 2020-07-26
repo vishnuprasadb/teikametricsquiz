c301403feacd0ed4fbc7c121b3b619cd3156a7c0
flo <prasadbajadka@gmail.com>
2020-07-26 16:04:59 +0530

    Build using browserHitory

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,20 @@ import React from 'react';
 import { ThemeProvider } from "@material-ui/core/styles";
 
 import theme from "./components/ui/Theme";
-import { Switch, Route, HashRouter } from "react-router-dom";
+import { Switch, Route, HashRouter, BrowserRouter } from "react-router-dom";
 
 import DashIndex from "./components/Quiz/DashIndex"
 import AssessmentIndex from "./components/Quiz/AssessmentIndex"
 import FinishIndex from "./components/Quiz/FinishIndex"
 import AllResults from "./components/Quiz/AllResultsIndex"
 
+const projectName:string = "/teikametricsquiz"
+
 const App: React.FC = () => {
   return (
     <div className="Quiz">
       <ThemeProvider theme={theme} >
-        <HashRouter>
+        <BrowserRouter>
           <Switch>
             <Route exact path = "/" component={DashIndex} />
             <Route exact path = "/test" component={AssessmentIndex} />
@@ -22,7 +24,7 @@ const App: React.FC = () => {
             <Route exact path = "/finish" component={FinishIndex} />
             <Route exact path = "/allresults" component={AllResults} />
           </Switch>
-        </HashRouter>       
+        </BrowserRouter>       
       </ThemeProvider>
     </div>
   );
